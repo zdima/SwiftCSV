c0d574f282f92517729fbaef2a86023c7d3c6a7c
Christian Tietze <christian.tietze@gmail.com>
2016-10-25 12:44:01 +0200

    make CSV generic over View

--- SwiftCSV/CSV.swift
+++ SwiftCSV/CSV.swift
@@ -8,80 +8,34 @@
 
 import Foundation
 
-public enum Variant {
-    case named
-    case enumerated
-}
-
 public protocol View {
-    associatedtype Rows
+    associatedtype Row
     associatedtype Columns
 
-    var rows: Rows { get }
+    var rows: [Row] { get }
     var columns: Columns { get }
 
-    init(header: [String], text: String, delimiter: Character, limitTo: Int?, loadColumns: Bool) throws
+    init(header: [String], text: String, delimiter: Character, loadColumns: Bool) throws
+
+    func serialize(header: [String], delimiter: Character) -> String
 }
 
-open class CSV {
+open class CSV<DataView : View>  {
     static fileprivate var comma: Character { return "," }
     
     open let header: [String]
 
-    lazy var _namedView: NamedView = {
-        return try! NamedView(
-            header: self.header,
-            text: self.text,
-            delimiter: self.delimiter,
-            loadColumns: self.loadColumns)
-    }()
+    let text: String
+    let delimiter: Character
 
-    lazy var _enumeratedView: EnumeratedView = {
-        return try! EnumeratedView(
-            header: self.header,
-            text: self.text,
-            delimiter: self.delimiter,
-            loadColumns: self.loadColumns)
-    }()
+    let content: DataView
 
-    var text: String
-    var delimiter: Character
-
-    let loadColumns: Bool
-
-    /// List of dictionaries that contains the CSV data
-    public var namedRows: [[String : String]] {
-        return _namedView.rows
+    public var rows: [DataView.Row] {
+        return content.rows
     }
 
-    /// Dictionary of header name to list of values in that column
-    /// Will not be loaded if loadColumns in init is false
-    public var namedColumns: [String : [String]] {
-        return _namedView.columns
-    }
-
-    /// Collection of column fields that contain the CSV data
-    public var enumeratedRows: [[String]] {
-        return _enumeratedView.rows
-    }
-
-    /// Collection of columns with metadata.
-    /// Will not be loaded if loadColumns in init is false
-    public var enumeratedColumns: [EnumeratedView.Column] {
-        return _enumeratedView.columns
-    }
-    
-
-
-
-    @available(*, unavailable, renamed: "namedRows")
-    public var rows: [[String : String]] {
-        return namedRows
-    }
-
-    @available(*, unavailable, renamed: "namedColumns")
-    public var columns: [String : [String]] {
-        return namedColumns
+    public var columns: DataView.Columns {
+        return content.columns
     }
 
     
@@ -90,11 +44,12 @@ open class CSV {
     /// - parameter string: Contents of the CSV file
     /// - parameter delimiter: Character to split row and header fields by (default is ',')
     /// - parameter loadColumns: Whether to populate the columns dictionary (default is true)
-    public init(string: String, variant: Variant = .named, delimiter: Character = comma, loadColumns: Bool = true) throws {
+    public init(string: String, delimiter: Character = comma, loadColumns: Bool = true) throws {
         self.text = string
         self.delimiter = delimiter
-        self.loadColumns = loadColumns
         self.header = try Parser.array(text: string, delimiter: delimiter, limitTo: 1).first ?? []
+
+        self.content = try DataView.init(header: header, text: text, delimiter: delimiter, loadColumns: loadColumns)
     }
     
     /// Load a CSV file
@@ -103,10 +58,10 @@ open class CSV {
     /// - parameter delimiter: character to split row and header fields by (default is ',')
     /// - parameter encoding: encoding used to read file (default is UTF-8)
     /// - parameter loadColumns: whether to populate the columns dictionary (default is true)
-    public convenience init(name: String, variant: Variant = .named, delimiter: Character = comma, encoding: String.Encoding = .utf8, loadColumns: Bool = true) throws {
+    public convenience init(name: String, delimiter: Character = comma, encoding: String.Encoding = .utf8, loadColumns: Bool = true) throws {
         let contents = try String(contentsOfFile: name, encoding: encoding)
     
-        try self.init(string: contents, variant: variant, delimiter: delimiter, loadColumns: loadColumns)
+        try self.init(string: contents, delimiter: delimiter, loadColumns: loadColumns)
     }
     
     /// Load a CSV file from a URL
@@ -115,14 +70,20 @@ open class CSV {
     /// - parameter delimiter: character to split row and header fields by (default is ',')
     /// - parameter encoding: encoding used to read file (default is UTF-8)
     /// - parameter loadColumns: whether to populate the columns dictionary (default is true)
-    public convenience init(url: URL, variant: Variant = .named, delimiter: Character = comma, encoding: String.Encoding = .utf8, loadColumns: Bool = true) throws {
+    public convenience init(url: URL, delimiter: Character = comma, encoding: String.Encoding = .utf8, loadColumns: Bool = true) throws {
         let contents = try String(contentsOf: url, encoding: encoding)
         
-        try self.init(string: contents, variant: variant, delimiter: delimiter, loadColumns: loadColumns)
+        try self.init(string: contents, delimiter: delimiter, loadColumns: loadColumns)
     }
     
     /// Turn the CSV data into NSData using a given encoding
     open func dataUsingEncoding(_ encoding: String.Encoding) -> Data? {
         return description.data(using: encoding)
+    }
+}
+
+extension CSV: CustomStringConvertible {
+    public var description: String {
+        return self.content.serialize(header: self.header, delimiter: self.delimiter)
     }
 }
